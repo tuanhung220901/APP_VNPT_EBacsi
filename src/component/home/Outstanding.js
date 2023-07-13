1eccc53f9ccbaa2cfb3a044a3d6a1fc8cc8d4ecd
Tuan Hung <hoangtuanhung220901@gmail.com>
2023-07-13 23:45:13 +0700

    update homePage

---
+++ src/component/home/Outstanding.js
@@ -0,0 +1,31 @@
+const Outstanding = ()=>{
+    return(
+        <div className="max-w-screen-xl mx-auto pt-20 pb-20">
+        <h2 className="text-center text-[32px] text-[#054a85] font-bold pb-12">SẢN PHẨM - DỊCH VỤ NỔI BẬT</h2>
+        <div className="flex flex-wrap items-center justify-around">
+            <div className="w-[30%] rounded-lg shadow-xl pb-6">
+                <img src="https://ebacsi.com.vn/assets/img/goikham/xetNghiemKhangThe.jpg" className="rounded-t-lg"/>
+                <div className="px-4 pt-3">
+                    <h3 className="text-[18px] text-[#0880e8] font-bold pb-3">GÓI KHÁM SỨC KHỎE HẬU COVID</h3>
+                    <p className="text-[15px] text-[#777777] leading-7 text-justify">Xét nghiệm kháng thể Covid-19 là xét nghiệm định lượng kháng thể ngừa virus Covid-19 trong máu , giúp Bác sĩ đánh giá nồng độ kháng thể ...</p>
+                </div>
+            </div>
+            <div className="w-[30%] rounded-lg shadow-xl pb-6">
+                <img src="https://ebacsi.com.vn/assets/img/goikham/hauCovid.jpg" className="rounded-t-lg"/>
+                <div className="px-4 pt-3">
+                    <h3 className="text-[18px] text-[#0880e8] font-bold pb-3">GÓI KHÁM SỨC KHỎE HẬU COVID</h3>
+                    <p className="text-[15px] text-[#777777] leading-7 text-justify">Gói kiểm tra sức khỏe hậu Covid-19 nâng cao giúp đánh giá, chẩn đoán, điều trị và dự phòng các yếu tố nguy cơ của hội chứng hậu Covid-19</p>
+                </div>
+            </div>
+            <div className="w-[30%] rounded-lg shadow-xl pb-6">
+                <img src="https://ebacsi.com.vn/assets/img/goikham/xetNghiemKhangThe.jpg" className="rounded-t-lg"/>
+                <div className="px-4 pt-3">
+                    <h3 className="text-[18px] text-[#0880e8] font-bold pb-3">GÓI KHÁM SỨC KHỎE HẬU COVID</h3>
+                    <p className="text-[15px] text-[#777777] leading-7 text-justify">Xét nghiệm kháng thể Covid-19 là xét nghiệm định lượng kháng thể ngừa virus Covid-19 trong máu , giúp Bác sĩ đánh giá nồng độ kháng thể ...</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+export default Outstanding;
