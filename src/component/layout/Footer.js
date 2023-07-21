8facb22625fae0e23216afdedae4ea5698e37805
Tuan Hung <hoangtuanhung220901@gmail.com>
2023-07-21 09:13:37 +0700

    update page about, service, blog, help

--- src/component/layout/Footer.js
+++ src/component/layout/Footer.js
@@ -20,9 +20,12 @@ const Footer = () =>{
                         <div className="w-[50%]">
                             <h4 className="text-[16px] font-bold">ĐIỀU KHOẢN VÀ CHÍNH SÁCH BẢO MẬT</h4>
                             <ul>
-                                <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">></span> Điều khoản hoạt động</li>
+                                {/* <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">></span> Điều khoản hoạt động</li>
                                 <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">></span> Quy định thanh toán</li>
-                                <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">></span> Chính sách bảo mật thông tin</li>
+                                <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">></span> Chính sách bảo mật thông tin</li> */}
+                                <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">-</span> Điều khoản hoạt động</li>
+                                <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">-</span> Quy định thanh toán</li>
+                                <li className="py-2.5 text-gray-300"> <span className="text-[#0084cb]">-</span> Chính sách bảo mật thông tin</li>
                             </ul>
                             <img src="https://ebacsi.com.vn/assets/img/dathongbao.png"/>
                         </div>
